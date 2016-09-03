520d3d60af1c8e6714975e8b5c0e234d3d98eb23
JV-BugMaker <794241063@qq.com>
2016-09-03 20:29:40 +0800

    [+]使用express创建blog

---
+++ blog/help.js
@@ -0,0 +1,14 @@
+//help js 帮助理解blogjs一些帮助文档
+app.js启动文件，入口文件
+package.json 存储着工程的信息及模块以来信息
+node_modules/ 存放package.json中安装的模块
+public/ 存放着img css js文件
+routes/存放着路径文件
+views/存放着视图文件
+bin/存放可执行文件
+
+//www文件中的第一行 表明是node可执行文件
+//ejs中使用
+<% code %> 存放js代码
+<%=code%> 显示替换过HTML特殊字符的内容哦
+<%-code%>显示原始HTML内容
