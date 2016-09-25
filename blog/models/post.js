aa8d46abe25cb8499aa088a9310001c6ca4f7b23
JV-BugMaker <794241063@qq.com>
2016-09-25 15:12:59 +0800

    [*]完善文章数据操作逻辑代码

--- blog/models/post.js
+++ blog/models/post.js
@@ -3,3 +3,88 @@
 *文章模型
 *对文章数据处理
  */
+var mongodb = require('./db');
+
+function Post(name,title,post)
+{
+    this.name = name;
+    this.title = title;
+    this.post = post;
+}
+
+
+module.exports = Post;
+
+Post.prototype.save = function(callback){
+    var date = new Date();
+    //存储各种时间格式、方便以后扩展
+    var time = {
+        date:date,
+        year:date.getFullYear(),
+        month:date.getFullYear() + "-" + (date.getMonth() + 1),
+        day:date.getFullYear() + "-" + (date.getMonth() + 1) + '-' + date.getDate(),
+        minute:date.getFullYear() + "-" + (date.getMonth() + 1) + '-' + date.getDate() + ' ' + date.getHours() + ':' + (date.getMinutes()<10?'0'+date.getMinutes():date.getMinutes())
+    };
+    //要存入数据库的文档
+    var post = {
+        name:this.name,
+        title:this.title,
+        time:time,
+        post:this.post
+    };
+
+    //打开数据库
+    mongodb.open(function(err,db){
+        if(err){
+            return callback(err);
+        }
+        //读取 posts 集合
+        db.collection('posts',function(err,collection){
+            if(err){
+                mongodb.close();
+                return callback(err);
+            }
+            //将文档插入到集合中
+            collection.insert(post,{
+                safe:true
+            },function(err){
+                mongodb.close();
+                if(err){
+                    return callback(err);
+                }
+                callback(null); //插入成功返回null
+            });
+        });
+    });
+};
+//读取文章以及相关信息
+Post.get = function(name,callback){
+    mongodb.open(function(err,db){
+      if(err){
+          return callback(err);
+      }
+      //读取集合
+      db.collection('posts',function(err,collection){
+          if(err){
+              mongodb.close();
+              return callback(err);
+          }
+          var query = {};
+          if(name){
+              query.name = name;
+          }
+          //根据query对象查询数据
+          collection.find(query).sort({
+              time:-1
+          }).toArray(function(err,docs){
+              //插入完成 关闭数据库以及后续结果操作处理
+              mongodb.close();
+              if(err){
+                  return callback(err);
+              }
+              //插入很共返回null 和文档
+              callback(null,docs);
+          });
+      });
+    });
+};
