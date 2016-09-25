e177117a22ef9843bc040ebaac3a5d89a353c08f
JV-BugMaker <794241063@qq.com>
2016-09-25 12:12:24 +0800

    [*]修改路由增加是否登录判断以及登录操作登出操作

--- blog/routes/index.js
+++ blog/routes/index.js
@@ -11,11 +11,23 @@ router.get('/', function(req, res, next) {
 //module.exports = router;
 module.exports = function(app){
   app.get('/',function(req,res){
-      res.render('index',{title:'主页'});
+      res.render('index',{
+        title:'主页',
+        user:req.session.user,
+        success:req.flash('success').toString(),
+        error:req.flash('error').toString(),
+      });
   });
+  app.get('/reg',checkNotLogin);
   app.get('/reg',function(req,res){
-      res.render('reg',{title:'注册'});
+      res.render('reg',{
+        title:'注册',
+        user:req.session.user,
+        success:req.flash('success').toString(),
+        error:req.flash('error').toString()
+      });
   });
+  app.post('/reg',checkNotLogin);
   app.post('/reg',function(req,res){
       var name = req.body.name,
           password = req.body.password,
@@ -52,16 +64,70 @@ module.exports = function(app){
           });
       });
   });
+  app.get('/login',checkNotLogin);
   app.get('/login',function(req,res){
-      res.render('login',{title:'登录'});
+      res.render('login',{
+        title:'登录',
+        user:req.session.user,
+        success:req.flash('success').toString(),
+        error:req.flash('error').toString()
+      });
   });
+  app.post('/login',checkNotLogin);
   app.post('/login',function(req,res){
+    //处理登录操作
+    var md5 = crypto.createHash('md5'),
+        password = md5.update(req.body.password).digest('hex');
+    //检查用户是否存在
+    User.get(req.body.name,function(err,user){
+        if(!user){
+            req.flash('用户不存在！');
+            //用户不存在，则跳转到登录页面
+            return res.redirect('/login');
+        }
+        if(password != user.password){
+            req.flash('密码错误!');
+            //密码错误则跳转到登录界面
+            return res.redirect('/login');
+        }
+        //过了前面两个验证之后 登录成功
+        req.session.user = user;
+        req.flash('success','登录成功');
+        res.redirect('/');
+    });
   });
+  app.get('/post',checkLogin);
   app.get('/post',function(req,res){
-      res.render('post',{title:'发布'});
+      res.render('post',{
+        title:'发布',
+        user:req.session.user,
+        success:req.flash('success').toString(),
+        error:req.flash('error').toString()
+      });
   });
   app.post('/post',function(req,res){
+
   });
+  app.get('/logout',checkLogin);
   app.get('/logout',function(req,res){
+      req.session.user = null;
+      req.flash('success','登出成功');
+      res.redirect('/');
   });
+  function checkLogin(req,res,next)
+  {
+      if(!req.session.user){
+          req.flash('error','未登录');
+          res.redirect('/login');
+      }
+      next();
+  }
+  function checkNotLogin(req,res,next)
+  {
+      if(req.session.user){
+          req.flash('error','已登录');
+          res.redirect('back');
+      }
+      next();
+  }
 };
