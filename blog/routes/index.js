26789b0198652ecf563511048a076e2e1b0857c0
JV-BugMaker <794241063@qq.com>
2016-10-22 19:18:20 +0800

    [+]增加留言功能的逻辑代码

--- blog/routes/index.js
+++ blog/routes/index.js
@@ -1,6 +1,7 @@
 var crypto = require('crypto');
 var User = require('../models/user.js');
 var Post = require('../models/post.js');
+var Comment = require('../models/comment.js');
 var express = require('express');
 var router = express.Router();
 
@@ -187,6 +188,28 @@ module.exports = function(app){
               });
           });
   });
+app.post('/u/:name/:day/:title',function(req,res){
+    var date = new Date(),
+        time = date.getFullYear() + "-" + (date.getMonth() + 1) + "-" + date.getDate() + "" + date.getHours() + ":" + (date.getMinutes() < 10 ?'0'+date.getMinutes():date.getMinutes());
+    var comment = {
+        name:req.body.name,
+        email:req.body.email,
+        website:req.body.website,
+        time:time,
+        content:req.body.content
+    };
+    var newComent = new Comment(req.params.name,req.params.day,req.params.title,comment);
+    newComent.save(function(err){
+        if(err){
+            req.flash('error',err);
+            return res.redirect;
+        }
+        req.flash('success','留言成功');
+        res.redirect('back');
+    });
+});
+
+
   app.get("/edit/:name/:day/:title",checkLogin);
   app.post("/edit/:name/:day/:title",function(req,res){
       //提交修改数据处理
